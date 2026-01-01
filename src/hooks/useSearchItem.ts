3b19e1397c86b12e3a72daf9db1927daccef0067
Jang-YuJin <jangyj0912@naver.com>
2026-01-01 21:06:17 +0900

    에러 주석

--- src/hooks/useSearchItem.ts
+++ src/hooks/useSearchItem.ts
@@ -2,22 +2,22 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { SearchRequest } from "../models/search";
 
 const useSearchItem = (params: SearchRequest) => {
-    const clientCredentialToken = useClientCredentialToken();
+    // const clientCredentialToken = useClientCredentialToken();
 
-    return useInfiniteQuery({
-        queryKey: ['search-item', params],
-        queryFn: ({pageParam: 0}) => {
-            if(!clientCredentialToken){
-                throw new Error('No token available');
-            }
+    // return useInfiniteQuery({
+    //     queryKey: ['search-item', params],
+    //     queryFn: ({pageParam: 0}) => {
+    //         if(!clientCredentialToken){
+    //             throw new Error('No token available');
+    //         }
 
-            return searchItem(clientCredentialToken, {...params, offset: pageParam});
-        },
-        initialPageParam: 0,
-        getNextPageParam: (lastPage) => {
+    //         return searchItem(clientCredentialToken, {...params, offset: pageParam});
+    //     },
+    //     initialPageParam: 0,
+    //     getNextPageParam: (lastPage) => {
 
-        }
-    })
+    //     }
+    // })
 };
 
 export default useSearchItem;
